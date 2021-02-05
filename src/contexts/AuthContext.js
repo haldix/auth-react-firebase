0652fa02fe8d432c95f0a447c0ecbc47d4730439
haldix <hjdpirate@yahoo.com>
2021-02-05 18:21:44 -0500

    initial commit

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+
+  const signup = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  useEffect(() => {
+    const unsubscibe = auth.onAuthStateChanged((user) => setCurrentUser(user));
+
+    return unsubscibe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
